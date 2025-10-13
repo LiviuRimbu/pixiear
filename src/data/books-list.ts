dba000fa162ddd0d4481556eb655f5ce8831b4ce
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2025-10-13 13:43:47 +0300

    change AR iframe

--- src/data/books-list.ts
+++ src/data/books-list.ts
@@ -5,8 +5,8 @@ export interface books {
 }
 
 export const books: books[] = [
-    {id: 0, src: 'https://mywebar.com/p/Project_0_qu59hfweju18028869', imageSrc: '/images/english.webp'},
+    {id: 0, src: 'https://mywebar.com/p/englishSmartInter', imageSrc: '/images/english.webp'},
     // {id: 1, src: 'https://mywebar.com/p/Project_7_87chzb90ir33865911', imageSrc: '/images/english2.webp'},
-    {id: 1, src: 'https://mywebar.com/p/Project_4_6k8la19ohc75691675', imageSrc: '/images/grammar.webp'},
+    {id: 1, src: 'https://mywebar.com/p/grammar', imageSrc: '/images/grammar.webp'},
     {id: 2, src: 'https://mywebar.com/p/Project_7_87chzb90ir33869729', imageSrc: '/images/france.webp'},
 ];
