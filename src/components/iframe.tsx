111d22562cd4a0c8d31e51a5d6c748110be6b807
LiviuRimbu <145881286+LiviuRimbu@users.noreply.github.com>
2026-03-20 16:54:22 +0200

    New Animations

--- src/components/iframe.tsx
+++ src/components/iframe.tsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { useTranslations } from 'next-intl';
+import React, { useState, useEffect } from "react";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 import InfoHeader from "@/components/info-header";
 import { books } from "@/data/books-list";
@@ -9,25 +9,25 @@ import { useIdAndLocales } from "@/lib/useIdAndLocales";
 import { Button } from "@/components/ui/button";
 
 const IframeComponent: React.FC = () => {
-    const t = useTranslations('iframe');
-    const { id, currentLocale } = useIdAndLocales();
-    const router = useRouter();
+  const t = useTranslations("iframe");
+  const { id, currentLocale } = useIdAndLocales();
+  const router = useRouter();
 
-    const [showButton, setShowButton] = useState(false);
-    const [fadeIn, setFadeIn] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  const [fadeIn, setFadeIn] = useState(false);
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setShowButton(true);
-            setTimeout(() => setFadeIn(true), 100);
-        }, 20000);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowButton(true);
+      setTimeout(() => setFadeIn(true), 100);
+    }, 20000);
 
-        return () => clearTimeout(timer);
-    }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-    return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-custom-gradient">
-            <iframe
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-custom-gradient">
+      {/* <iframe
                 src={books[Number(id)].src}
                 frameBorder="0"
                 scrolling="yes"
@@ -35,27 +35,48 @@ const IframeComponent: React.FC = () => {
                 style={{ display: 'block', width: '100vw', height: '100vh' }}
                 allow="camera;gyroscope;accelerometer;magnetometer;xr-spatial-tracking;microphone"
                 title="Project Iframe"
-            ></iframe>
+            ></iframe> */}
+      <iframe
+        src={books[Number(id)].src}
+        allow="camera; gyroscope; accelerometer; magnetometer; xr-spatial-tracking; autoplay; fullscreen"
+        loading="eager"
+        referrerPolicy="origin"
+        title="AR Scene"
+        style={{
+          display: "block",
+          width: "100vw",
+          height: "100vh",
+          border: "none",
+          overflow: "hidden",
+        }}
+      />
+      <InfoHeader
+        btnText={t("button")}
+        imageSrc={books[Number(id)].imageSrc}
+        imgText={t("selected-image")}
+        navigationWay={`${currentLocale}/book-detail/id=${id}/`}
+      />
 
-            <InfoHeader
-                btnText={t('button')}
-                imageSrc={books[Number(id)].imageSrc}
-                imgText={t('selected-image')}
-                navigationWay={`${currentLocale}/book-detail/id=${id}/`}
-            />
-
-            {showButton && (
-                <Button
-                    className={`absolute hover:scale-105 active:scale-105 bottom-10 px-12 py-6 font-bold tracking-wider rounded-full bg-gradient-to-r from-blue-500 via-red-900 to-yellow-400 bg-[length:200%] animate-gradient transition-all duration-4000 
-                    transition-opacity duration-1000 ease-in-out ${fadeIn ? 'opacity-100' : 'opacity-0'}`}
-                    onClick={() => router.push(`/${currentLocale}/book-detail/id=${id}/qrcode`)}
-                >
-                    <Image src="/images/qr-code-white.webp" alt='qr code' width={20} height={20} className="mr-5" />
-                    <p className="text-2xl text-white">{t('button-to-qr')}</p>
-                </Button>
-            )}
-        </div>
-    );
+      {showButton && (
+        <Button
+          className={`absolute hover:scale-105 active:scale-105 bottom-10 px-12 py-6 font-bold tracking-wider rounded-full bg-gradient-to-r from-blue-500 via-red-900 to-yellow-400 bg-[length:200%] animate-gradient transition-all duration-4000 
+                    transition-opacity duration-1000 ease-in-out ${fadeIn ? "opacity-100" : "opacity-0"}`}
+          onClick={() =>
+            router.push(`/${currentLocale}/book-detail/id=${id}/qrcode`)
+          }
+        >
+          <Image
+            src="/images/qr-code-white.webp"
+            alt="qr code"
+            width={20}
+            height={20}
+            className="mr-5"
+          />
+          <p className="text-2xl text-white">{t("button-to-qr")}</p>
+        </Button>
+      )}
+    </div>
+  );
 };
 
 export default IframeComponent;
--- src/data/books-list.ts
+++ src/data/books-list.ts
@@ -1,12 +1,24 @@
 export interface books {
-    id: number;
-    src: string;
-    imageSrc: string;
+  id: number;
+  src: string;
+  imageSrc: string;
 }
 
 export const books: books[] = [
-    {id: 0, src: 'https://mywebar.com/p/englishSmartInter', imageSrc: '/images/english.webp'},
-    // {id: 1, src: 'https://mywebar.com/p/Project_7_87chzb90ir33865911', imageSrc: '/images/english2.webp'},
-    {id: 1, src: 'https://mywebar.com/p/grammar', imageSrc: '/images/grammar.webp'},
-    {id: 2, src: 'https://mywebar.com/p/Project_7_87chzb90ir33869729', imageSrc: '/images/france.webp'},
+  {
+    id: 0,
+    src: "https://mywebar.com/p/englishSmartInter2",
+    imageSrc: "/images/english.webp",
+  },
+  // {id: 1, src: 'https://mywebar.com/p/Project_7_87chzb90ir33865911', imageSrc: '/images/english2.webp'},
+  {
+    id: 1,
+    src: "https://mywebar.com/p/grammar-2",
+    imageSrc: "/images/grammar.webp",
+  },
+  {
+    id: 2,
+    src: "https://mywebar.com/p/franceza-3",
+    imageSrc: "/images/france.webp",
+  },
 ];
