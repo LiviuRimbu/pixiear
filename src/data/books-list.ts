16ace7c743b6fbb39fd88f9d1d28e18162679333
LiviuRimbu <liviurimbu@theunit.dev>
2026-09-05 23:59:38 +0300

    Corrected the MyWebAR link

--- src/data/books-list.ts
+++ src/data/books-list.ts
@@ -3,12 +3,13 @@ export interface Book {
   src: string;
   imageSrc: string;
 }
-
+//https://github.com/LiviuRimbu/pixiear
+//https://github.com/LiviuRimbu/pixieAR-test
 export const books: Book[] = [
-  { id: 0, src: 'https://mywebar.com/1', imageSrc: '/images/english.webp' },
+  { id: 0, src: 'https://mywebar.com/p/englishSmartInter2', imageSrc: '/images/english.webp' },
   { id: 1, src: 'https://mywebar.com/p/Project_7_87chzb90ir33865911', imageSrc: '/images/english2.webp' },
-  { id: 2, src: 'https://mywebar.com/2', imageSrc: '/images/grammar.webp' },
+  { id: 2, src: 'https://mywebar.com/p/grammar-2', imageSrc: '/images/grammar.webp' },
   { id: 3, src: 'https://mywebar.com/p/Project_25_h8qc2gbhwl', imageSrc: '/images/grammar-clone.webp' },
-  { id: 4, src: 'https://mywebar.com/3', imageSrc: '/images/france.webp' },
+  { id: 4, src: 'https://mywebar.com/p/franceza-33', imageSrc: '/images/france.webp' },
 
 ];
